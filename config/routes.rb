56116a246ef84c8e9167eff217c6397666293115
Samuellekpor <samuellekpor@gmail.com>
2023-04-16 01:27:11 +0000

    Create a new controller and route to handle the API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      get 'messages/random', to: 'messages#random'
+    end
+  end
 end
